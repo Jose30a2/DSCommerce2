97dc11d44915dafad831c1b133b57f7ebea71252
Jose Antonio Perez <jose30a2@gmail.com>
2026-01-02 11:49:50 -0300

    OrderItem

--- src/main/java/com/jose30a2/dscommerce/entities/Order.java
+++ src/main/java/com/jose30a2/dscommerce/entities/Order.java
@@ -3,6 +3,9 @@ package com.jose30a2.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 
 @Entity
@@ -24,6 +27,9 @@ public class Order {
 
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order(){}
 
@@ -74,4 +80,14 @@ public class Order {
     public void setPayment(Payment payment) {
         this.payment = payment;
     }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    // Obter los productos que estan dentro del Order
+    public List<Product> getProducts(){
+        return items.stream().map(x -> x.getProduct()).toList();
+    }
+
 }
